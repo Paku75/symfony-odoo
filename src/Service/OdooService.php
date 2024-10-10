929d3fba230e52a92638463fac33bfb74e0480f7
Pham Tien Quyen <35308084+Paku75@users.noreply.github.com>
2024-10-10 12:29:21 +0200

    Optimise API calls

--- src/Service/OdooService.php
+++ src/Service/OdooService.php
@@ -1,27 +1,56 @@
 <?php
 namespace App\Service;
 
+use Symfony\Component\HttpFoundation\RequestStack;
+
 class OdooService
 {
   private HttpService $httpService;
+  private RequestStack $requestStack;
   private string $odooUrl;
   private string $odooDb;
   private string $odooUser;
   private string $odooPassword;
 
-  public function __construct(HttpService $httpService, string $odooUrl, string $odooDb, string $odooUser, string $odooPassword)
-  {
+  public function __construct(
+    HttpService $httpService,
+    RequestStack $requestStack,
+    string $odooUrl,
+    string $odooDb,
+    string $odooUser,
+    string $odooPassword
+  ) {
     $this->httpService  = $httpService;
+    $this->requestStack = $requestStack;
     $this->odooUrl      = $odooUrl;
     $this->odooDb       = $odooDb;
     $this->odooUser     = $odooUser;
     $this->odooPassword = $odooPassword;
   }
 
+  private function getSession()
+  {
+    return $this->requestStack->getCurrentRequest()->getSession();
+  }
+
   private function odooAuthenticate()
   {
-    // Returns uid for odoo calls
-    return $this->httpService->odooCall($this->odooUrl, 'common', 'authenticate', [$this->odooDb, $this->odooUser, $this->odooPassword, []]);
+    $session = $this->getSession();
+
+    if ($session->has('odoo_uid')) {
+      return $session->get('odoo_uid');
+    }
+
+    $uid     = $this->httpService->odooCall($this->odooUrl, 'common', 'authenticate', [
+      $this->odooDb,
+      $this->odooUser,
+      $this->odooPassword,
+      []
+    ]);
+
+    $session->set('odoo_uid', $uid);
+
+    return $uid;
   }
 
   public function getOdooModelData($model, $context = [], $fields = [])
